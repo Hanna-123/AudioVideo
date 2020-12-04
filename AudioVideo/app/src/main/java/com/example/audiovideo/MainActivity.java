ea2aea6fa00666043f923f5353ddc2bd4bab98b5
Hanna <shadowemo13@hotmail.com>
2020-12-04 16:50:17 -0600

    funcionando

--- AudioVideo/app/src/main/java/com/example/audiovideo/MainActivity.java
+++ AudioVideo/app/src/main/java/com/example/audiovideo/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.audiovideo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity  implements View.OnClickListener{
+    Button jc01, jc02, jc03, jc04, jv01;
+    Bundle bdl;
+    Intent itn;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
         setContentView(R.layout.activity_main);
+        jc01 = (Button) findViewById(R.id.xbt1);
+        jc02 = (Button) findViewById(R.id.xbt2);
+        jc03 = (Button) findViewById(R.id.xbt3);
+        jc04 = (Button) findViewById(R.id.xbt4);
+        jv01 = (Button) findViewById(R.id.xbt5);
+        jc01.setOnClickListener(this);
+        jc02.setOnClickListener(this);
+        jc03.setOnClickListener(this);
+        jc04.setOnClickListener(this);
+        jv01.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v){
+        String tipo = "";
+        Button b = (Button)v;
+        int nombreCancion = 0;
+        if(v.getId() == R.id.xbt5) {
+            tipo = "video";
+        }
+        else{
+            if (v.getId() == R.id.xbt1) {
+                nombreCancion = 1;
+            }
+            if (v.getId() == R.id.xbt2) {
+                nombreCancion = 2;
+            }
+            if (v.getId() == R.id.xbt3) {
+                nombreCancion = 3;
+            }
+            if (v.getId() == R.id.xbt4) {
+                nombreCancion = 4;
+            }
+            tipo = "cancion";
+        }
+
+        itn = new Intent( MainActivity.this, SecondaryClass.class);
+        bdl = new Bundle();
+        bdl.putString("TIPO", tipo);
+        bdl.putString("NOMBRE", b.getText().toString());
+        bdl.putInt("NOMBRECANCION",nombreCancion);
+        itn.putExtras(bdl);
+        startActivity(itn);
+
     }
 }
---
+++ AudioVideo/app/src/main/java/com/example/audiovideo/SecondaryClass.java
@@ -0,0 +1,59 @@
+package com.example.audiovideo;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.*;
+public class SecondaryClass extends Activity {
+    TextView jtv1;
+    Bundle bdl;
+    MediaPlayer mp;
+    VideoView vvw;
+    Uri uri;
+    MediaController mcr;
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.second_activity);
+        jtv1 = (TextView)findViewById(R.id.xtv1);
+        bdl = getIntent().getExtras();
+        String nombre = bdl.getString("NOMBRE");
+        String tipo = bdl.getString("TIPO");
+        int nombreCancion = bdl.getInt("NOMBRECANCION");
+        jtv1.append("Reproduciendo: "+ nombre);
+        if (nombreCancion == 1){
+            reproducir(R.raw.adele_hello);
+        }
+        if (nombreCancion == 2){
+            reproducir(R.raw.ahhh);
+        }
+        if (nombreCancion == 3){
+            reproducir(R.raw.aplauso);
+        }
+        if (nombreCancion == 4){
+            reproducir(R.raw.you_got_it);
+        }
+        if (nombreCancion == 0){
+            vvw = (VideoView) findViewById(R.id.xvv1);
+            uri = Uri.parse("android.resource://com.example.audiovideo/"+
+                    R.raw.this_is_love);
+            mcr = new MediaController(this);
+            vvw.setMediaController(mcr);
+            vvw.setVideoURI(uri);
+            vvw.start();
+        }
+
+    }
+    void reproducir(int cancion){
+        if(mp != null) mp.release();
+        mp = MediaPlayer.create(this, cancion);
+        mp.seekTo(0);
+        mp.start();
+
+    }
+    public void onPause(){
+        super.onPause();
+        if(mp != null)
+            mp.release();
+    }
+}
